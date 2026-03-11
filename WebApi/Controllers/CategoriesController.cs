7afd4fcbcbe8fccb0ee0d1ec380eb191cc93ef5e
yuns-emre <yunstnrv@gmail.com>
2026-03-11 11:14:54 +0300

    Add Category and Chef Controllers

---
+++ WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Context;
+using WebApi.Entities;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public CategoriesController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetCategories()
+        {
+            var categories = _context.Categories.ToList();
+            return Ok(categories);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetCategoryById(int id)
+        {
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound("Kategori bulunamadı.");
+            }
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory([FromBody] Category category)
+        {
+            if (category == null)
+            {
+                return BadRequest("Category cannot be null.");
+            }
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return Ok("Kategori ekleme işlemi başarılı");
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult UpdateCategory(int id, [FromBody] Category category)
+        {
+            var existingCategory = _context.Categories.FirstOrDefault(c => c.Id == id);
+            if (existingCategory == null)
+            {
+                return NotFound("Kategori bulunamadı.");
+            }
+            existingCategory.Name = category.Name;
+            _context.SaveChanges();
+            return Ok("Kategori güncelleme işlemi başarılı");
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult DeleteCategory(int id)
+        {
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound("Kategori bulunamadı.");
+            }
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            return Ok("Kategori silme işlemi başarılı");
+        }
+    }
+}
---
+++ WebApi/Controllers/ChefsController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Context;
+using WebApi.Entities;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChefsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public ChefsController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetChefs()
+        {
+            var chefs = _context.Chefs.ToList();
+            return Ok(chefs);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetChef(int id)
+        {
+            var chef = _context.Chefs.Find(id);
+            if (chef == null)
+            {
+                return NotFound();
+            }
+            return Ok(chef);
+        }
+
+        [HttpPost]
+        public IActionResult CreateChef(Chef chef)
+        {
+            _context.Chefs.Add(chef);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetChef), new { id = chef.Id }, chef);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateChef(int id, Chef chef)
+        {
+            var existingChef = _context.Chefs.FirstOrDefault(c => c.Id == id);
+            if (existingChef == null)
+            {
+                return NotFound("Kategori bulunamadı.");
+            }
+            existingChef.NameSurname = chef.NameSurname;
+            existingChef.Description = chef.Description;
+            existingChef.ImageUrl = chef.ImageUrl;
+
+            _context.Entry(existingChef).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+            return Ok("Başarılı");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteChef(int id)
+        {
+            var chef = _context.Chefs.Find(id);
+            if (chef == null)
+            {
+                return NotFound();
+            }
+            _context.Chefs.Remove(chef);
+            _context.SaveChanges();
+            return Ok("Başarılı");
+        }
+
+    }
+
+}
